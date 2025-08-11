cd2d362c7bd68fd405681c0f564cd7bfddb2d5ff
BrendaPadilha10 <161397607+BrendaPadilha10@users.noreply.github.com>
2025-08-11 19:01:02 -0300

    001

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
-  {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-  {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-];
 {
     enunciado: "Pergunta 1",
     alternativas: [
@@ -61,6 +50,10 @@ const perguntas = [
 ] 
 let atual = 0;
 let perguntaAtual;
+
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
+
+mostraPergunta();

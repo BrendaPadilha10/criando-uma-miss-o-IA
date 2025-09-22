c6db1ec45485c24fa806e0ab74f75669d5bf0350
BrendaPadilha10 <161397607+BrendaPadilha10@users.noreply.github.com>
2025-09-22 19:09:32 -0300

    555

--- script.js
+++ script.js
@@ -19,29 +19,27 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
+        enunciado: "O que mais influencia sua escolha por um cosmético?",
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "O que mais influencia sua escolha por um cosmético?",
+                afirmacao: "Você prioriza produtos que cuidam da saúde da sua pele a longo prazo."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Preço acessível",
+                afirmacao: "Você busca equilíbrio entre beleza e economia, preferindo opções mais em conta.",
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Onde você costuma comprar seus cosméticos?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Lojas físicas",
+                afirmacao: "Você prefere experimentar os produtos antes de comprar e receber orientação direta."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
+                texto: "Lojas online",
+                afirmacao: "Você valoriza praticidade e variedade na hora de escolher cosméticos."
         ]
     },
     {

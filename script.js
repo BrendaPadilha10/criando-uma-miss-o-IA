62498bcf49c3d3f8f3286a4ed38cca65e536550c
BrendaPadilha10 <161397607+BrendaPadilha10@users.noreply.github.com>
2025-09-22 19:03:09 -0300

    001

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Com que frequência você utiliza cosméticos no seu dia a dia?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Diariamente",
+                afirmacao: "Isso indica que os cosméticos fazem parte da sua rotina de cuidados pessoais."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Apenas em ocasiões especiais",
+                afirmacao: "Você valoriza o uso pontual de cosméticos, priorizando naturalidade no dia a dia."
             }
         ]
     },
